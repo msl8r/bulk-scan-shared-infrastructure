c63098241c65e31919dc7c72915a1dbc30ddc96c
donatas M <1420191+doncem@users.noreply.github.com>
2018-12-10 14:16:59 +0000

    Create queue for notifications (#50)
    * Create queue for notifications
    
    * Remove restriction for duplicates

--- queue.tf
+++ queue.tf
@@ -17,10 +17,25 @@ module "queue" {
   duplicate_detection_history_time_window = "PT1H"
 }
 
+module "notification-queue" {
+  source              = "git@github.com:hmcts/terraform-module-servicebus-queue.git"
+  name                = "notifications"
+  namespace_name      = "${module.queue-namespace.name}"
+  resource_group_name = "${azurerm_resource_group.rg.name}"
+}
+
 output "queue_primary_listen_connection_string" {
   value = "${module.queue.primary_listen_connection_string}"
 }
 
 output "queue_primary_send_connection_string" {
   value = "${module.queue.primary_send_connection_string}"
+}
+
+output "notification_primary_listen_connection_string" {
+  value = "${module.notification-queue.primary_listen_connection_string}"
+}
+
+output "notification_primary_send_connection_string" {
+  value = "${module.notification-queue.primary_send_connection_string}"
 }
