ec3cff5466329892f489129d1ca0b3ab0f55bca4
luigibk <41112628+luigibk@users.noreply.github.com>
2018-12-12 10:51:23 +0000

    Importer env (#53)
    * Update ignore file
    
    * Move more parameters to env vars
    
    * Move env vars to docker compose file
    
    * Fix quoting

--- docker/ccd-definition-import/scripts/idam-authenticate.sh
+++ docker/ccd-definition-import/scripts/idam-authenticate.sh
@@ -4,8 +4,9 @@ IMPORTER_USERNAME=$1
 IMPORTER_PASSWORD=$2
 IDAM_URI=$3
 REDIRECT_URI=$4
-CLIENT_SECRET=$5
+CLIENT_ID=$5
+CLIENT_SECRET=$6
 
-code=$(curl ${CURL_OPTS} -u "${IMPORTER_USERNAME}:${IMPORTER_PASSWORD}" -XPOST "${IDAM_URI}/oauth2/authorize?redirect_uri=${REDIRECT_URI}&response_type=code&client_id=bsp" | jq -r .code)
+code=$(curl ${CURL_OPTS} -u "${IMPORTER_USERNAME}:${IMPORTER_PASSWORD}" -XPOST "${IDAM_URI}/oauth2/authorize?redirect_uri=${REDIRECT_URI}&response_type=code&client_id=${CLIENT_ID}" | jq -r .code)
 
-curl ${CURL_OPTS} -H "Content-Type: application/x-www-form-urlencoded" -u "bsp:${CLIENT_SECRET}" -XPOST "${IDAM_URI}/oauth2/token?code=${code}&redirect_uri=${REDIRECT_URI}&grant_type=authorization_code" | jq -r .access_token
+curl ${CURL_OPTS} -H "Content-Type: application/x-www-form-urlencoded" -u "${CLIENT_ID}:${CLIENT_SECRET}" -XPOST "${IDAM_URI}/oauth2/token?code=${code}&redirect_uri=${REDIRECT_URI}&grant_type=authorization_code" | jq -r .access_token
--- docker/ccd-definition-import/scripts/upload-definition.sh
+++ docker/ccd-definition-import/scripts/upload-definition.sh
@@ -3,25 +3,18 @@ set -e
 
 chmod +x /scripts/*.sh
 
-IMPORTER_USERNAME=ccd-importer@server.net
-IMPORTER_PASSWORD=Password12
-IDAM_URI=http://idam-api:8080
-REDIRECT_URI=http://localhost:3000/receiver
-CLIENT_SECRET=123456
-BULK_SCAN_ORCHESTRATOR_BASE_URL=http://host.docker.internal:8582
-
 if [ ${VERBOSE} = "true" ]; then
   export CURL_OPTS="-v"
 else
   export CURL_OPTS="--fail --silent"
 fi
 
-/scripts/create-importer-user.sh ${IMPORTER_USERNAME} ${IMPORTER_PASSWORD}
+[ "_${CREATE_IMPORTER_USER}" = "_yes" ] && /scripts/create-importer-user.sh ${IMPORTER_USERNAME} ${IMPORTER_PASSWORD}
 
-userToken=$(sh ./scripts/idam-authenticate.sh ${IMPORTER_USERNAME} ${IMPORTER_PASSWORD} ${IDAM_URI} ${REDIRECT_URI} ${CLIENT_SECRET})
+userToken=$(sh ./scripts/idam-authenticate.sh ${IMPORTER_USERNAME} ${IMPORTER_PASSWORD} ${IDAM_URI} ${REDIRECT_URI} ${CLIENT_ID} ${CLIENT_SECRET})
 
 # add ccd role
-/scripts/add-ccd-role.sh "caseworker-bulkscan" "PUBLIC" "${userToken}"
+/scripts/add-ccd-role.sh "${CCD_ROLE}" "PUBLIC" "${userToken}"
 
 for definition in /definitions/C*.xlsx # use C to not process excel temp files that start with ~
 do
