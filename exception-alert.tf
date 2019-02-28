ac225123bf5853eaf6a2df509cec9e4877e15375
donatas M <1420191+doncem@users.noreply.github.com>
2019-02-28 10:50:35 +0000

    Temporary enable alerts on aat (#73)

--- exception-alert.tf
+++ exception-alert.tf
@@ -4,7 +4,7 @@ module "bulk-scan-exception-alert" {
   location          = "${azurerm_application_insights.appinsights.location}"
   app_insights_name = "${azurerm_application_insights.appinsights.name}"
 
-  enabled    = "${var.env == "prod"}"
+  enabled    = "${var.env == "prod" || var.env == "aat"}"
   alert_name = "Bulk_Scan_exception_-_BSP"
   alert_desc = "Triggers when bulk scan services receive at least one exception within a 15 minutes window timeframe."
 
