ec7890c1abdadf352bb108c312d0860c5b3253b9
Rafal Kondratowicz <rkondratowicz@users.noreply.github.com>
2019-04-18 17:05:12 +0200

    Add storage container for cmc (#100)

--- storage-account.tf
+++ storage-account.tf
@@ -10,7 +10,7 @@ locals {
 
   // for each client service two containers are created: one named after the service
   // and another one, named {service_name}-rejected, for storing envelopes rejected by bulk-scan
-  client_service_names = ["bulkscan", "sscs", "divorce", "probate", "finrem"]
+  client_service_names = ["bulkscan", "sscs", "divorce", "probate", "finrem", "cmc"]
 }
 
 data "azurerm_subnet" "trusted_subnet" {
