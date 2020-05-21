21831b2ea3e9538a72446aa89400ff9d1f057852
Nitin Prabhu <nitinprabhu@users.noreply.github.com>
2020-05-21 17:44:29 +0100

    Used waf exclusion rules(REQUEST-920-PROTOCOL-ENFORCEMENT) branch (#167)

--- app-gateway.tf
+++ app-gateway.tf
@@ -9,7 +9,7 @@ data "azurerm_key_vault_secret" "cert" {
 }
 
 module "appGw" {
-  source            = "git@github.com:hmcts/cnp-module-waf?ref=master"
+  source            = "git@github.com:hmcts/cnp-module-waf?ref=CHG5001024"
   env               = "${var.env}"
   subscription      = "${var.subscription}"
   location          = "${var.location}"
