bc8bfa29346a4d40ff1bd5846e9735c8b52eb91f
James Johnson <2965841+jrjohnson87@users.noreply.github.com>
2020-10-05 17:17:51 +0100

    Update app-gateway-staging.tf

--- app-gateway-staging.tf
+++ app-gateway-staging.tf
@@ -1,5 +1,5 @@
 module "appGwStaging" {
-  source            = "git@github.com:hmcts/cnp-module-waf?ref=master"
+  source            = "git@github.com:hmcts/cnp-module-waf?ref=CHG5001024"
   env               = "${var.env}"
   subscription      = "${var.subscription}"
   location          = "${var.location}"
