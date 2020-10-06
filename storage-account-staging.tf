806fe029e9dc81d593dc89a4c4dcb665ec62412c
James Johnson <2965841+jrjohnson87@users.noreply.github.com>
2020-10-06 01:34:42 +0100

    Update storage-account-staging.tf

--- storage-account-staging.tf
+++ storage-account-staging.tf
@@ -59,18 +59,6 @@ resource "azurerm_storage_account" "storage_account_staging" {
   tags = "${local.tags}"
 }
 
-resource "azurerm_storage_container" "service_containers_stg" {
-  name                 = "${local.client_service_names_stg[count.index]}"
-  storage_account_name = "${azurerm_storage_account.storage_account_staging.*.name}"
-  count                = "${length(local.client_service_names_stg)}"
-}
-
-resource "azurerm_storage_container" "service_rejected_containers_stg" {
-  name                 = "${local.client_service_names_stg[count.index]}-rejected"
-  storage_account_name = "${azurerm_storage_account.storage_account_staging.*.name}"
-  count                = "${length(local.client_service_names_stg)}"
-}
-
 resource "azurerm_key_vault_secret" "storage_account_staging_name" {
   count        = local.stage
   key_vault_id = "${module.vault.key_vault_id}"
