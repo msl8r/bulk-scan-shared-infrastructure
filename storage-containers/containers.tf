4b380951fb94e00e0a2a6decbfc92aaa9a719d48
James Johnson <2965841+jrjohnson87@users.noreply.github.com>
2020-10-06 01:38:53 +0100

    Update containers.tf

--- storage-containers/containers.tf
+++ storage-containers/containers.tf
@@ -1,11 +1,19 @@
+variable "storage_account_name" {
+  type = string
+}
+
+variable "client_service_names" {
+  type = list(string)
+}
+
 resource "azurerm_storage_container" "service_containers_stg" {
   name                 = "${local.client_service_names_stg[count.index]}"
-  storage_account_name = "${azurerm_storage_account.storage_account_staging.*.name}"
-  count                = "${length(local.client_service_names_stg)}"
+  storage_account_name = "${var.storage_account_name}"
+  count                = "${length(var.client_service_names_stg)}"
 }
 
 resource "azurerm_storage_container" "service_rejected_containers_stg" {
-  name                 = "${local.client_service_names_stg[count.index]}-rejected"
-  storage_account_name = "${azurerm_storage_account.storage_account_staging.*.name}"
-  count                = "${length(local.client_service_names_stg)}"
+  name                 = "${var.client_service_names_stg[count.index]}-rejected"
+  storage_account_name = "${var.storage_account_name}"
+  count                = "${length(var.client_service_names_stg)}"
 }
