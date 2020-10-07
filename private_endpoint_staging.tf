d62b01c77e0fd79be8e0e4668973d4c16d304c14
James Johnson <2965841+jrjohnson87@users.noreply.github.com>
2020-10-07 12:15:05 +0100

    Update private_endpoint_staging.tf

--- private_endpoint_staging.tf
+++ private_endpoint_staging.tf
@@ -1,16 +1,15 @@
-resource "azurerm_template_deployment" "private_endpoint_staging" {
-  count               = "${var.env == "aat" ? "1": "0"}"
-  name                = "${local.account_name}staging-endpoint"
+resource "azurerm_template_deployment" "private_endpoint-staging" {
+  name                = "${local.account_name}-endpoint"
   resource_group_name = "${azurerm_resource_group.rg.name}"
 
   template_body = file("private_endpoint_template.json")
 
   parameters = {
-    endpoint_name       = "${local.account_name}staging-endpoint"
+    endpoint_name       = "${local.account_name}-endpoint"
     endpoint_location   = "${azurerm_resource_group.rg.location}"
     subnet_id           = "${data.azurerm_subnet.scan_storage_subnet.id}"
-    storageaccount_id   = "${azurerm_storage_account.storage_account.id}" 
-    storageaccount_fqdn = "${azurerm_storage_account.storage_account.primary_blob_endpoint}"
+    storageaccount_id   = "${azurerm_storage_account.storage_account_staging.id}" 
+    storageaccount_fqdn = "${azurerm_storage_account.storage_account_staging.primary_blob_endpoint}"
   }
 
   deployment_mode = "Incremental"
