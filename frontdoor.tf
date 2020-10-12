46602c7f5283fb627c4e2f973482b0b45bb52b0c
James Johnson <2965841+jrjohnson87@users.noreply.github.com>
2020-10-12 16:41:43 +0100

    Update frontdoor.tf

--- frontdoor.tf
+++ frontdoor.tf
@@ -38,10 +38,36 @@ resource "azurerm_frontdoor" "frontdoor" {
 
   frontend_endpoint {
     name                              = "storageFrontendEndpoint"
+    host_name                         = "bulkscan.demo.platform.hmcts"
+    custom_https_provisioning_enabled = true
+    
+    custom_https_configuration {
+      custom_https_configuration = "AzureKeyVault"
+      azure_key_vault_certificate_vault_id = data.azurerm_key_vault.infra_vault.id
+      azure_key_vault_certificate_secret_name = "${var.external_cert_name}"
+      azure_key_vault_certificate_secret_version = data.azurerm_key_vault_secret.cert.version  
+    }
+  }
+    
+  frontend_endpoint {
+    name                              = "defaultFrontendEndpoint"
     host_name                         = "${var.product}-${var.env}-FrontDoor.azurefd.net"
     custom_https_provisioning_enabled = false
   }
 }
+
+data "azurerm_key_vault" "infra_vault" {
+  name = "infra-vault-${var.subscription}"
+  resource_group_name = "${var.env == "prod" ? "core-infra-prod" : "cnp-core-infra"}"
+}
+
+
+data "azurerm_key_vault_secret" "cert" {
+  name      = "${var.external_cert_name}"
+  name      = "${var.external_cert_name}"
+  key_vault_id = "${data.azurerm_key_vault.infra_vault.id}"
+}
+
 
 resource "azurerm_frontdoor_firewall_policy" "wafpolicy" {
   name                              = "bulkscan${replace(var.env, "-", "")}wafpolicy"
