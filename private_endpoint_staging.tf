a755d1c4bb61b3bdea386ba100ce0ebb041e8618
Tim Jacomb <tim.jacomb@hmcts.net>
2020-11-06 16:40:14 +0000

    Convert private endpoint to terraform (#211)

--- private_endpoint_staging.tf
+++ private_endpoint_staging.tf
@@ -1,16 +1,13 @@
-resource "azurerm_template_deployment" "private_endpoint_staging" {
+resource "azurerm_private_endpoint" "private_endpoint_stg" {
   name                = "${local.account_name}stg-endpoint"
-  resource_group_name = "${azurerm_resource_group.rg.name}"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  subnet_id           = data.azurerm_subnet.scan_storage_subnet.id
 
-  template_body = file("private_endpoint_template.json")
-
-  parameters = {
-    endpoint_name       = "${local.account_name}stg-endpoint"
-    endpoint_location   = "${azurerm_resource_group.rg.location}"
-    subnet_id           = "${data.azurerm_subnet.scan_storage_subnet.id}"
-    storageaccount_id   = "${azurerm_storage_account.storage_account_staging.id}" 
-    storageaccount_fqdn = "${azurerm_storage_account.storage_account_staging.primary_blob_endpoint}"
+  private_service_connection {
+    name                           = "${local.account_name}stg-endpoint"
+    private_connection_resource_id = azurerm_storage_account.storage_account_staging.id
+    is_manual_connection           = false
+    subresource_names              = ["blob", "blob_secondary"]
   }
-
-  deployment_mode = "Incremental"
 }
