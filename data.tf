37441f90abc1c41c01ad5a2f75820014eec6aadc
MUSTAFA <mustafa.dayican@hmcts.net>
2021-08-05 12:35:20 +0100

    DTSBPS-699 add new aat vnet to blob storages (#236)

--- data.tf
+++ data.tf
@@ -3,9 +3,12 @@ locals {
   mgmt_network_rg_name = "aks-infra-cftptl-intsvc-rg"
   
   aks_env = var.env == "sandbox" ? "sbox" : var.env
-  
-  app_aks_network_name = "core-${local.aks_env}-vnet"
-  app_aks_network_rg_name = "aks-infra-${local.aks_env}-rg"
+
+  aat_cft_vnet_name           = "cft-aat-vnet"
+  aat_cft_vnet_resource_group = "cft-aat-network-rg"
+
+  app_aks_network_name = var.env ==  "aat" ? local.aat_cft_vnet_name : "core-${local.aks_env}-vnet"
+  app_aks_network_rg_name =  var.env ==  "aat" ? local.aat_cft_vnet_resource_group : "aks-infra-${local.aks_env}-rg"
 }
 
 data "azurerm_subnet" "jenkins_subnet" {
