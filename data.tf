c582556e182cdfa7887333adb8abab1c1bfe2da2
mdayican <mustafa.dayican@hmcts.net>
2021-08-05 12:14:39 +0100

    fix

--- data.tf
+++ data.tf
@@ -4,11 +4,8 @@ locals {
   
   aks_env = var.env == "sandbox" ? "sbox" : var.env
 
-  aat_cft_vnet_name           = "cft-aat-vnet"
-  aat_cft_vnet_resource_group = "cft-aat-network-rg"
-
-  app_aks_network_name = var.env ==  "aat" ? aat_cft_vnet_name : "core-${local.aks_env}-vnet"
-  app_aks_network_rg_name =  var.env ==  "aat" ? aat_cft_vnet_resource_group : "aks-infra-${local.aks_env}-rg"
+  app_aks_network_name = var.env ==  "aat" ? "cft-aat-vnet" : "core-${local.aks_env}-vnet"
+  app_aks_network_rg_name =  var.env ==  "aat" ? "cft-aat-network-rg" : "aks-infra-${local.aks_env}-rg"
 }
 
 data "azurerm_subnet" "jenkins_subnet" {
