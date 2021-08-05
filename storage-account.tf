c854dc3463750344427dd6e8a0808526497e1099
mdayican <mustafa.dayican@hmcts.net>
2021-08-05 11:41:26 +0100

    reformat

--- storage-account.tf
+++ storage-account.tf
@@ -23,7 +23,12 @@ locals {
   ]
 
   preview_subnets = var.env == "aat"
-  ? [data.azurerm_subnet.preview_aks_00_subnet.id, data.azurerm_subnet.preview_aks_01_subnet.id, data.azurerm_subnet.aat_cft_aks_00_subnet.id,data.azurerm_subnet.aat_cft_aks_01_subnet.id] : []
+  ? [
+    data.azurerm_subnet.preview_aks_00_subnet.id,
+    data.azurerm_subnet.preview_aks_01_subnet.id,
+    data.azurerm_subnet.aat_cft_aks_00_subnet.id,
+    data.azurerm_subnet.aat_cft_aks_01_subnet.id
+  ]: []
 
   all_valid_subnets =  concat(local.valid_subnets, local.preview_subnets)
 
