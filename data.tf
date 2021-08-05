106cdaa21645730566996c3479d791eab29cebcd
mdayican <mustafa.dayican@hmcts.net>
2021-08-05 12:20:43 +0100

    use local

--- data.tf
+++ data.tf
@@ -4,8 +4,11 @@ locals {
   
   aks_env = var.env == "sandbox" ? "sbox" : var.env
 
-  app_aks_network_name = var.env ==  "aat" ? "cft-aat-vnet" : "core-${local.aks_env}-vnet"
-  app_aks_network_rg_name =  var.env ==  "aat" ? "cft-aat-network-rg" : "aks-infra-${local.aks_env}-rg"
+  aat_cft_vnet_name           = "cft-aat-vnet"
+  aat_cft_vnet_resource_group = "cft-aat-network-rg"
+
+  app_aks_network_name = var.env ==  "aat" ? local.aat_cft_vnet_name : "core-${local.aks_env}-vnet"
+  app_aks_network_rg_name =  var.env ==  "aat" ? local.aat_cft_vnet_resource_group : "aks-infra-${local.aks_env}-rg"
 }
 
 data "azurerm_subnet" "jenkins_subnet" {
