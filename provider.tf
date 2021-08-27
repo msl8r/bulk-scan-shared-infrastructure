222ced5891f7251d22fcb2ed567260ff05d59f5c
mdayican <mustafa.dayican@hmcts.net>
2021-08-27 09:29:19 +0100

    revert all - fix broken terraform changes

--- provider.tf
+++ provider.tf
@@ -1,16 +1,5 @@
 terraform {
   backend "azurerm" {}
-
-  required_providers {
-    azuread = {
-      source  = "hashicorp/azuread"
-      version = "1.6.0"
-    }
-    random = {
-      source = "hashicorp/random"
-    }
-  }
-
 }
 
 provider "azurerm" {
