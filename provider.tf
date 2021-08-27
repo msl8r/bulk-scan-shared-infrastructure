1b43c4ce02e62ab7894fbbadaf8a6ae7636ecdf5
mdayican <mustafa.dayican@hmcts.net>
2021-08-27 09:23:59 +0100

    fix broken terraform-6

--- provider.tf
+++ provider.tf
@@ -6,10 +6,6 @@ terraform {
       source  = "hashicorp/azuread"
       version = "1.6.0"
     }
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "=2.72.0"
-    }
     random = {
       source = "hashicorp/random"
     }
