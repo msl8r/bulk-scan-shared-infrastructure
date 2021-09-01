515c637f854df02525aead4d8cd8321097c40154
mdayican <mustafa.dayican@hmcts.net>
2021-09-01 07:34:53 +0100

    fix terraform issue provider issue-4

--- state.tf
+++ state.tf
@@ -4,11 +4,12 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 2.25"
+      version = "=2.72.0"
     }
     azuread = {
       source  = "hashicorp/azuread"
       version = "1.6.0"
     }
+
   }
 }
