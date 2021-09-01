299210a34677732100eafa6a0bf6119d984a1ca6
mdayican <mustafa.dayican@hmcts.net>
2021-09-01 07:49:27 +0100

    fix terraform -3

--- state.tf
+++ state.tf
@@ -6,9 +6,8 @@ terraform {
       source  = "hashicorp/azurerm"
       version = "=2.49.0"
     }
-    azuread = {
-      source  = "hashicorp/azuread"
-      version = "1.6.0"
+    random = {
+      source = "hashicorp/random"
     }
   }
 }
