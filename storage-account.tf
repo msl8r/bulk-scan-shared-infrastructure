642ba9da759d61080b78361b3872f817408be055
Andrey Pereverzin <andrey.pereverzin@hmcts.net>
2022-01-14 10:27:36 +0000

    Created privatelaw container

--- storage-account.tf
+++ storage-account.tf
@@ -10,7 +10,7 @@ locals {
 
   // for each client service two containers are created: one named after the service
   // and another one, named {service_name}-rejected, for storing envelopes rejected by bulk-scan
-  client_service_names = ["bulkscanauto", "bulkscan", "sscs", "divorce", "probate", "finrem", "cmc", "publiclaw"]
+  client_service_names = ["bulkscanauto", "bulkscan", "sscs", "divorce", "probate", "finrem", "cmc", "publiclaw", "privatelaw"]
 
   resourcegroup_name          = "${azurerm_resource_group.rg.name}"
 
